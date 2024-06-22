12e9baef53d8e2da630cb05f4d09646e155b5257
Juniortru21 <139279189+Juniortru21@users.noreply.github.com>
2024-06-22 10:42:45 -0500

    Delete folder and remove storage connection

--- Application/UseCases/Transactions/Queries/GetTransactions/GetTransactionsQuery.cs
+++ Application/UseCases/Transactions/Queries/GetTransactions/GetTransactionsQuery.cs
@@ -1,8 +1,4 @@
-﻿using System.Diagnostics;
-using System.IO;
-using Microsoft.Extensions.Configuration;
-using Application.Common.Interfaces;
-using Application.Common.Utils;
+﻿using Application.Common.Interfaces;
 using Application.UseCases.Common.Handlers;
 using Application.UseCases.Common.Results;
 using Domain.Entities;
@@ -12,28 +8,11 @@ namespace Application.UseCases.Transactions.Queries.GetTransactions;
 public class GetTransactionsQuery : IRequest<Result<GetTransactionsQueryDto>>
 {
     public class GetTransactionsQueryHandler(
-        IConfiguration configuration,
         IRepository<Transaction> transaccionRepository) : UseCaseHandler, IRequestHandler<GetTransactionsQuery, Result<GetTransactionsQueryDto>>
     {
         public async Task<Result<GetTransactionsQueryDto>> Handle(GetTransactionsQuery request, CancellationToken cancellationToken)
         {
-            var path = configuration.GetSection("FilePath").Value;
-            var fileName = configuration.GetSection("FileName").Value ?? string.Empty;
-            var extension = configuration.GetSection("Extension").Value ?? string.Empty;
-
             var transactions = await transaccionRepository.GetAllAsync();
-
-            transactions.ToList().ForEach(async x =>
-            {
-                await Document<Transaction>.SaveToCsv(transactions.Where(y => y == x), $"{path}{fileName}{x.File}.{extension}");
-
-                var fileStream = Document<Transaction>.ImportCsv($"{path}{fileName}{x.File}.{extension}");
-
-                var blobStorage = new BlobStorageService(configuration);
-
-                await blobStorage.UploadFile($"{fileName}{x.File}", extension, fileStream);
-                await blobStorage.DownloadFile();
-            });
 
             var transactionsDto = transactions
                     .Select(x => new GetTransactionsQueryValueDto()
