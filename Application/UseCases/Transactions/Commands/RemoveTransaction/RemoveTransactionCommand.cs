4c07761433a5afa2bcc92db6b599ab37d1c65842
AlbertoTrujillo0221 <57775191+AlbertoTrujillo0221@users.noreply.github.com>
2024-06-22 09:39:12 -0500

    Add files via upload

---
+++ Application/UseCases/Transactions/Commands/RemoveTransaction/RemoveTransactionCommand.cs
@@ -0,0 +1,26 @@
+﻿using Application.Common.Interfaces;
+using Application.UseCases.Common.Handlers;
+using Application.UseCases.Common.Results;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.UseCases.Transactions.Commands.DeleteTransaction;
+public class RemoveTransactionCommand : RemoveTransactionCommandModel, IRequest<Result<RemoveTransactionCommandDto>>
+{
+    public class DeleteTransactionCommandHandler(
+        IRepository<Transaction> transactionRepository) : UseCaseHandler, IRequestHandler<RemoveTransactionCommand, Result<RemoveTransactionCommandDto>>
+    {
+        public async Task<Result<RemoveTransactionCommandDto>> Handle(RemoveTransactionCommand request, CancellationToken cancellationToken)
+        {
+            var transaction =
+                    await transactionRepository.GetByIdAsync(request.Id)
+                    ?? throw (new ArgumentException("The transaction id does not exist"));
+
+            await transactionRepository.RemoveAsync(transaction);
+
+            var resultData = new RemoveTransactionCommandDto { Success = true };
+
+            return this.Succeded(resultData);
+        }
+    }
+}
---
+++ Application/UseCases/Transactions/Queries/GetTransactions/GetTransactionsQuery.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics;
+using System.IO;
+using Microsoft.Extensions.Configuration;
+using Application.Common.Interfaces;
+using Application.Common.Utils;
+using Application.UseCases.Common.Handlers;
+using Application.UseCases.Common.Results;
+using Domain.Entities;
+using MediatR;
+namespace Application.UseCases.Transactions.Queries.GetTransactions;
+
+public class GetTransactionsQuery : IRequest<Result<GetTransactionsQueryDto>>
+{
+    public class GetTransactionsQueryHandler(
+        IConfiguration configuration,
+        IRepository<Transaction> transaccionRepository) : UseCaseHandler, IRequestHandler<GetTransactionsQuery, Result<GetTransactionsQueryDto>>
+    {
+        public async Task<Result<GetTransactionsQueryDto>> Handle(GetTransactionsQuery request, CancellationToken cancellationToken)
+        {
+            var path = configuration.GetSection("FilePath").Value;
+            var fileName = configuration.GetSection("FileName").Value ?? string.Empty;
+            var extension = configuration.GetSection("Extension").Value ?? string.Empty;
+
+            var transactions = await transaccionRepository.GetAllAsync();
+
+            transactions.ToList().ForEach(async x =>
+            {
+                await Document<Transaction>.SaveToCsv(transactions.Where(y => y == x), $"{path}{fileName}{x.File}.{extension}");
+
+                var fileStream = Document<Transaction>.ImportCsv($"{path}{fileName}{x.File}.{extension}");
+
+                var blobStorage = new BlobStorageService(configuration);
+
+                await blobStorage.UploadFile($"{fileName}{x.File}", extension, fileStream);
+                await blobStorage.DownloadFile();
+            });
+
+            var transactionsDto = transactions
+                    .Select(x => new GetTransactionsQueryValueDto()
+                    {
+                        Id = x.Id,
+                        Value = x.Value,
+                        Status = x.Status,
+                        Date = x.Date
+                    });
+
+            var resultData = new GetTransactionsQueryDto()
+            {
+                Transactions = transactionsDto
+            };
+
+            return this.Succeded(resultData);
+        }
+    }
+}
